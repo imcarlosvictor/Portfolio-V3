6d900c673bb09a143dab0e0deea6cd5ed4489c3c
Lucas <imcvlucas@gmail.com>
2023-11-20 22:02:41 -0500

    about me section complete

--- mysite/frontend/src/components/Home.jsx
+++ mysite/frontend/src/components/Home.jsx
@@ -9,11 +9,13 @@ export default function Home() {
         <div className="content">
 
           <div className="introduction-container">
-            <p className="greetings">Hi, I'm</p>
-            <p className="my-name">Carlos Victor Torres</p> 
-            <p className="aspiration">A <span id="profession">software developer</span> based in Toronto.</p>
-            <div className="info">
-              <a href="#project-header"><button id="to-projects">Projects</button></a>
+            <div className="introduction-content">
+              <p className="greetings">Hi, I'm</p>
+              <p className="my-name">Carlos Victor Torres</p> 
+              <p className="aspiration">A <span id="profession">software developer</span> based in Toronto.</p>
+              <div className="info">
+                <a href="#project-header"><button id="to-projects">Projects</button></a>
+              </div>
             </div>
           </div>
 
@@ -24,12 +26,11 @@ export default function Home() {
 
             <div className="about-me-content">
               <div id="about-me-section">
-                <p className="section-title section-about-me">Who am I</p>
                 <p>Greetings! I'm Carlos Victor, a self-taught software engineer with a background in UX Design. My passion is in building digital products from start to finish that focus on solving real world problems. <br/> <br/> After completing my capstone project for the UX Design course, I realized that my passion wasn't in design but in building products. Designing is fun, but, building products from scratch and solving complex problems along the way is far more fulfilling. I believe that most problems can be solved using technology. Therefore, I'm always       thrilled to spend my free time learning new languages, skills, and build projects to hone my craft. <br/> <br/> When I'm not coding, you can find me playing chess, video games, or learning game theory.
                 </p>
               </div>
+
               <div className="skill-section">
-                <p className="section-title section-skills">Skills</p>
                 <p>Here are a few technologies I've been working with recently:</p>
                 <div className="skills">
                   <ul>
