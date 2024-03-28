8d0822a6bc3ba9678620ee0eb669136b1503e9f8
Lucas <imcvlucas@gmail.com>
2024-03-27 23:36:33 -0400

    updated layout and theme

--- mysite/frontend/src/components/Home.jsx
+++ mysite/frontend/src/components/Home.jsx
@@ -73,61 +73,27 @@ export default function Home() {
         <div className="content">
 
           <div className="project-container">
-            <div className="project-content">
-              <div className="menu">
-
-                <div className="project-image">
-                  <iframe className="image" data-id="1" src='https://my.spline.design/molang3dcopy-14f433a2bec08affba8c9f97c0afcb3c/' frameBorder='0' width='100%' height='100%'></iframe>
-                  <iframe className="image" data-id="2" src='https://my.spline.design/miniroomartcopy-346d32b6b8167de57d38369b7c8cbebc/' frameBorder='0' width='100%' height='100%'></iframe>
-                  <iframe className="image" data-id="3" src='https://my.spline.design/molang3dcopy-14f433a2bec08affba8c9f97c0afcb3c/' frameBorder='0' width='100%' height='100%'></iframe>
-                  <iframe className="image" data-id="4" src='https://my.spline.design/molang3dcopy-14f433a2bec08affba8c9f97c0afcb3c/' frameBorder='0' width='100%' height='100%'></iframe>
-                  <iframe className="image" data-id="5" src='https://my.spline.design/molang3dcopy-14f433a2bec08affba8c9f97c0afcb3c/' frameBorder='0' width='100%' height='100%'></iframe>
-                  <iframe className="image" data-id="6" src='https://my.spline.design/molang3dcopy-14f433a2bec08affba8c9f97c0afcb3c/' frameBorder='0' width='100%' height='100%'></iframe>
-                  <iframe className="image" data-id="7" src='https://my.spline.design/molang3dcopy-14f433a2bec08affba8c9f97c0afcb3c/' frameBorder='0' width='100%' height='100%'></iframe>
-                  <iframe className="image" data-id="8" src='https://my.spline.design/molang3dcopy-14f433a2bec08affba8c9f97c0afcb3c/' frameBorder='0' width='100%' height='100%'></iframe>
-                  <iframe className="image" data-id="9" src='https://my.spline.design/molang3dcopy-14f433a2bec08affba8c9f97c0afcb3c/' frameBorder='0' width='100%' height='100%'></iframe>
-                </div>
-
-                <div className="project-link-container">
-                  <div className="project-link">
-                    <a href="#" data-image="1">Medusa*</a>
-                  </div>
-
-                  <div className="project-link">
-                    <a href="#" data-image="2">Unified Fugitive Database</a>
-                  </div>
-
-                  <div className="project-link">
-                    <a href="#" data-image="3">Pathfinding Algorithm Visualizer</a>
-                  </div>
-
-                  <div className="project-link">
-                    <a href="#" data-image="4">Car Rentals</a>
-                  </div>
-
-                  <div className="project-link">
-                    <a href="#" data-image="5">Weather Forecast</a>
-                  </div>
-
-                  <div className="project-link">
-                    <a href="#" data-image="6">GPU Price Scraper</a>
-                  </div>
-
-                  <div className="project-link">
-                    <a href="#" data-image="7">Stock Scraper</a>
-                  </div>
-
-                  <div className="project-link">
-                    <a href="#" data-image="8">Student Database Management System</a>
-                  </div>
-
-                  <div className="project-link">
-                    <a href="#" data-image="9">Adam Williamson Portfolio</a>
-                  </div>
-                </div>
-
+            <div className="project-category">
+              <div className="category-block">
+                <p>AI</p>
               </div>
-
+              <div className="category-block">
+                <p>Data</p>
+              </div>
+            </div>
+            <div className="project-link-container">
+              <div className="project-block">
+                <div className="project-poster">
+                </div>
+                <div className="project-details">
+                </div>
+              </div>
+              <div className="project-block">
+                <div className="project-poster">
+                </div>
+                <div className="project-details">
+                </div>
+              </div>
             </div>
           </div>
 
--- mysite/frontend/src/components/Project.jsx
+++ mysite/frontend/src/components/Project.jsx
@@ -27,40 +27,64 @@ export default function Project() {
 
         return (
           <>
-            <div className="project-info-landing-page">
-              <canvas id="gradient-canvas" data-transition-in />
-              <div className="project-info-title-container">
-                <div className="scrolling-text">
-                  <h1 id="project-info-title">medusa</h1>
+            <div className="content">
+
+              <div className="project-info-landing-page">
+                <canvas id="gradient-canvas" data-transition-in />
+                <div className="project-info-title-container">
+                  <div className="scrolling-text">
+                    {/* <h1 id="project-info-title">Title</h1> */}
+                  </div>
+                </div>
+
+                <div className="scrolldown">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+
+              </div>
+
+              <div className="project-overview">
+                <div className="project-title">
+                  <p>Medusa</p>
+                </div>
+                <div className="project-overview-content">
+                  <div className="project-overview-title">
+                    <p>Project<br></br>Overview</p>
+                  </div>
+                  <div className="project-overview-text">
+                    <p>
+                      Lorem ipsum <span className="bold">dolor sit amet</span>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse in est ante in. Fames ac turpis egestas maecenas pharetra convallis posuere. Lectus arcu bibendum at varius vel pharetra vel turpis. Lectus mauris ultrices eros in cursus turpis. Vitae tempus quam pellentesque nec. Pharetra diam sit amet nisl suscipit adipiscing bibendum est. Orci phasellus egestas tellus rutrum tellus pellentesque. Netus et malesuada fames ac turpis. Vestibulum lorem sed risus ultricies tristique. Sed risus pretium quam vulputate dignissim suspendisse in est. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi. Cursus risus at ultrices mi tempus imperdiet nulla. Eu sem integer vitae justo eget.
+
+              Vitae et leo duis ut diam quam nulla porttitor. Mattis aliquam faucibus purus in massa tempor nec feugiat. Risus pretium quam vulputate dignissim suspendisse in est ante. Mattis vulputate enim nulla aliquet. Arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales. Tellus at urna condimentum mattis pellentesque id nibh. Lectus nulla at volutpat diam ut venenatis. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Purus in massa tempor nec feugiat nisl. Ultricies integer quis auctor elit sed vulputate mi. Eu scelerisque felis imperdiet proin fermentum leo. Nunc pulvinar sapien et ligula ullamcorper malesuada proin. Odio ut enim blandit volutpat maecenas volutpat blandit aliquam. A iaculis at erat pellentesque adipiscing. Et malesuada fames ac turpis egestas sed tempus. Nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus. In nibh mauris cursus mattis molestie a.
+                    </p>
+                  </div>
                 </div>
               </div>
 
-              <div className="scrolldown">
-                <span></span>
-                <span></span>
-                <span></span>
+
+              <div className="project-info-content">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse in est ante in. Fames ac turpis egestas maecenas pharetra convallis posuere. Lectus arcu bibendum at varius vel pharetra vel turpis. Lectus mauris ultrices eros in cursus turpis. Vitae tempus quam pellentesque nec. Pharetra diam sit amet nisl suscipit adipiscing bibendum est. Orci phasellus egestas tellus rutrum tellus pellentesque. Netus et malesuada fames ac turpis. Vestibulum lorem sed risus ultricies tristique. Sed risus pretium quam vulputate dignissim suspendisse in est. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi. Cursus risus at ultrices mi tempus imperdiet nulla. Eu sem integer vitae justo eget.
+
+          Vitae et leo duis ut diam quam nulla porttitor. Mattis aliquam faucibus purus in massa tempor nec feugiat. Risus pretium quam vulputate dignissim suspendisse in est ante. Mattis vulputate enim nulla aliquet. Arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales. Tellus at urna condimentum mattis pellentesque id nibh. Lectus nulla at volutpat diam ut venenatis. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Purus in massa tempor nec feugiat nisl. Ultricies integer quis auctor elit sed vulputate mi. Eu scelerisque felis imperdiet proin fermentum leo. Nunc pulvinar sapien et ligula ullamcorper malesuada proin. Odio ut enim blandit volutpat maecenas volutpat blandit aliquam. A iaculis at erat pellentesque adipiscing. Et malesuada fames ac turpis egestas sed tempus. Nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus. In nibh mauris cursus mattis molestie a.
+
+          Sed odio morbi quis commodo odio aenean sed adipiscing. Et magnis dis parturient montes nascetur ridiculus mus. Quis vel eros donec ac odio. Imperdiet nulla malesuada pellentesque elit eget gravida cum. Est placerat in egestas erat imperdiet. Quisque id diam vel quam elementum. Amet cursus sit amet dictum. Odio facilisis mauris sit amet. Id interdum velit laoreet id donec ultrices. Tellus molestie nunc non blandit massa enim nec. Sed egestas egestas fringilla phasellus faucibus scelerisque. Ligula ullamcorper malesuada proin libero nunc consequat. Cursus vitae congue mauris rhoncus. Eu consequat ac felis donec et odio. Dictum at tempor commodo ullamcorper a. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Mollis nunc sed id semper.
+
+          Justo nec ultrices dui sapien. Id aliquet risus feugiat in ante metus dictum. Sit amet mattis vulputate enim nulla aliquet. In aliquam sem fringilla ut morbi. Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Enim sed faucibus turpis in eu mi bibendum. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Nec nam aliquam sem et tortor. Enim sit amet venenatis urna. Nisi scelerisque eu ultrices vitae auctor. Tempus iaculis urna id volutpat.
+
+          Eu tincidunt tortor aliquam nulla facilisi. Est sit amet facilisis magna etiam. Ultricies leo integer malesuada nunc vel risus commodo viverra maecenas. Malesuada proin libero nunc consequat interdum varius sit amet mattis. Enim nunc faucibus a pellentesque sit amet porttitor eget. Elementum tempus egestas sed sed. Risus nec feugiat in fermentum posuere urna nec. Erat imperdiet sed euismod nisi porta lorem mollis. Nisl purus in mollis nunc sed id semper. In ante metus dictum at. Condimentum id venenatis a condimentum vitae sapien. Id aliquet risus feugiat in ante metus dictum. Iaculis at erat pellentesque adipiscing commodo elit. Nisl suscipit adipiscing bibendum est ultricies integer quis auctor.
+                  </p>
               </div>
-            </div>
 
-            <div className="project-info-content">
-              <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Suspendisse in est ante in. Fames ac turpis egestas maecenas pharetra convallis posuere. Lectus arcu bibendum at varius vel pharetra vel turpis. Lectus mauris ultrices eros in cursus turpis. Vitae tempus quam pellentesque nec. Pharetra diam sit amet nisl suscipit adipiscing bibendum est. Orci phasellus egestas tellus rutrum tellus pellentesque. Netus et malesuada fames ac turpis. Vestibulum lorem sed risus ultricies tristique. Sed risus pretium quam vulputate dignissim suspendisse in est. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi. Cursus risus at ultrices mi tempus imperdiet nulla. Eu sem integer vitae justo eget.
-
-        Vitae et leo duis ut diam quam nulla porttitor. Mattis aliquam faucibus purus in massa tempor nec feugiat. Risus pretium quam vulputate dignissim suspendisse in est ante. Mattis vulputate enim nulla aliquet. Arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales. Tellus at urna condimentum mattis pellentesque id nibh. Lectus nulla at volutpat diam ut venenatis. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Purus in massa tempor nec feugiat nisl. Ultricies integer quis auctor elit sed vulputate mi. Eu scelerisque felis imperdiet proin fermentum leo. Nunc pulvinar sapien et ligula ullamcorper malesuada proin. Odio ut enim blandit volutpat maecenas volutpat blandit aliquam. A iaculis at erat pellentesque adipiscing. Et malesuada fames ac turpis egestas sed tempus. Nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus. In nibh mauris cursus mattis molestie a.
-
-        Sed odio morbi quis commodo odio aenean sed adipiscing. Et magnis dis parturient montes nascetur ridiculus mus. Quis vel eros donec ac odio. Imperdiet nulla malesuada pellentesque elit eget gravida cum. Est placerat in egestas erat imperdiet. Quisque id diam vel quam elementum. Amet cursus sit amet dictum. Odio facilisis mauris sit amet. Id interdum velit laoreet id donec ultrices. Tellus molestie nunc non blandit massa enim nec. Sed egestas egestas fringilla phasellus faucibus scelerisque. Ligula ullamcorper malesuada proin libero nunc consequat. Cursus vitae congue mauris rhoncus. Eu consequat ac felis donec et odio. Dictum at tempor commodo ullamcorper a. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Mollis nunc sed id semper.
-
-        Justo nec ultrices dui sapien. Id aliquet risus feugiat in ante metus dictum. Sit amet mattis vulputate enim nulla aliquet. In aliquam sem fringilla ut morbi. Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Enim sed faucibus turpis in eu mi bibendum. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Nec nam aliquam sem et tortor. Enim sit amet venenatis urna. Nisi scelerisque eu ultrices vitae auctor. Tempus iaculis urna id volutpat.
-
-        Eu tincidunt tortor aliquam nulla facilisi. Est sit amet facilisis magna etiam. Ultricies leo integer malesuada nunc vel risus commodo viverra maecenas. Malesuada proin libero nunc consequat interdum varius sit amet mattis. Enim nunc faucibus a pellentesque sit amet porttitor eget. Elementum tempus egestas sed sed. Risus nec feugiat in fermentum posuere urna nec. Erat imperdiet sed euismod nisi porta lorem mollis. Nisl purus in mollis nunc sed id semper. In ante metus dictum at. Condimentum id venenatis a condimentum vitae sapien. Id aliquet risus feugiat in ante metus dictum. Iaculis at erat pellentesque adipiscing commodo elit. Nisl suscipit adipiscing bibendum est ultricies integer quis auctor.
-                </p>
-            </div>
-
-            <div className="progress-section">
-              <div className="progress-bar-wrap">
-                <div className="progress-bar"></div>
+              <div className="progress-section">
+                <div className="progress-bar-wrap">
+                  <div className="progress-bar"></div>
+                </div>
+                <div className="progress-num"></div>
               </div>
-              <div className="progress-num"></div>
+
             </div>
           </>
         )
